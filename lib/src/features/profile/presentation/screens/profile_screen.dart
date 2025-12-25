b3af941f0398a5710c89f185ff979d9e974fdaf5
tarekfouda <tarekfouda0@gmail.com>
2025-12-25 12:23:32 +0200

    profile review

--- lib/src/features/profile/presentation/screens/profile_screen.dart
+++ lib/src/features/profile/presentation/screens/profile_screen.dart
@@ -1,8 +1,13 @@
 part of '../../profile_imports.dart';
 
-class ProfilePage extends StatelessWidget {
-  ProfilePage({Key? key}) : super(key: key);
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
 
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
